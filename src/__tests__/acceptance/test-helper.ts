2a86c8a0f7845905c12d7d7953353760d0043f5f
Nate Lantz <nate@webshield.io>
2019-04-30 13:05:51 -0700

    add lb4, add basic steps

---
+++ src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {DroneTestApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new DroneTestApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: DroneTestApplication;
+  client: Client;
+}
